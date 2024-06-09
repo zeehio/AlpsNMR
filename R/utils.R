1abc6e1d8bee02f1ae3825bc0354a9ee58b6171d
Sergio Oller <sergioller@gmail.com>
2024-06-09 09:42:51 +0200

    Move ASICS export to utils.R

--- R/utils.R
+++ R/utils.R
@@ -288,6 +288,31 @@ to_ChemoSpec <- function(nmr_dataset, desc = "A nmr_dataset", group = NULL) {
     return(Spectra)
 }
 
+#' @title Export data for the spectral quantification library ASICS  
+#' @description
+#' A simple helper function for mangling data in the right format for the 
+#' spectral quantification library ASICS. 
+#' 
+#' @param samples An `nmr_dataset_family` 1D object 
+#' @param ... Additional arguments are passed 
+#'            directly to `ASICS::createSpectra`, which (in theory) provide an 
+#'            opportunity to use distinct normalisation methods. 
+#' 
+#' @return An `ASICS::Spectra` object 
+#' @examples
+#' # forAsics <- alps_asics(dataset)
+#' # ASICS(forAsics)
+#' 
+#' @export 
+
+alps_asics <- function(samples, ...) {
+    forAsics <- t(nmr_data(samples))
+    forAsics <- ASICS::createSpectra(forAsics, ...)
+    forAsics@sample.name <-samples$metadata$external$NMRExperiment
+    return(forAsics)
+}
+
+
 
 abort_if_not <- function(condition, ...) {
     if (!condition) {
