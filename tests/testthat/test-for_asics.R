1169e266d66bb84d2699af030f870b9d72d85a20
Sergio Oller <sergioller@gmail.com>
2024-06-09 10:12:14 +0200

    Update tests according to autophase and asics changes

--- tests/testthat/test-for_asics.R
+++ tests/testthat/test-for_asics.R
@@ -1,8 +1,19 @@
-test_that("alps_asics works", {
+test_that("to_ASICS works", {
     skip_if_not_installed("ASICS")
-    dir_to_demo_dataset <- system.file("dataset-demo", package = "AlpsNMR")
-    dataset <- nmr_read_samples_dir(dir_to_demo_dataset)
-    dataset <- nmr_interpolate_1D(dataset, axis = c(min = 1, max = 2, by = 0.002))
-    spec_obj <- alps_asics(dataset)
-    expect_true(class(spec_obj)[1]=="Spectra")
+    # Create a random spectra matrix
+    nsamp <- 3
+    npoints <- 300
+    dummy_ppm_axis <- seq(from = 0.2, to = 10, length.out = npoints)
+    dummy_spectra_matrix <- matrix(runif(nsamp * npoints), nrow = nsamp, ncol = npoints)
+    metadata <- list(external = data.frame(
+        NMRExperiment = paste0("Sample", seq_len(nsamp)),
+        DummyClass = c("a", "b")[sample.int(2, nsamp, replace=TRUE)]
+    ))
+    dummy_nmr_dataset_1D <- new_nmr_dataset_1D(
+        ppm_axis = dummy_ppm_axis,
+        data_1r = dummy_spectra_matrix,
+        metadata = metadata
+    )
+    spec_obj <- to_ASICS(dummy_nmr_dataset_1D)
+    expect_true(class(spec_obj)[1] == "Spectra")
 })
